09093d7756dbea0a2b5a0bb4aa21b77853777f82
Julian Dragoi <juliand@reflection.io>
2018-07-16 10:20:03 +0100

    deleted old scripts

--- ApplianceType.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS ApplianceType (
-    ApplianceTypeId     INT PRIMARY KEY AUTOINCREMENT NOT NULL DEFAULT(-1),
-    Type                VARCHAR(200) NOT NULL,
-    Make                VARCHAR(200) NOT NULL,
-    Model               VARCHAR(200) NOT NULL,
-    GCNumber            VARCHAR(20)   
-)
--- Job.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS Job (
-    JobId                   INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL DEFAULT(-1),
-    FirstName               VARCHAR(128),
-    Surname                 VARCHAR(128),
-    FirstLineAddress        VARCHAR(128),
-    SecondLineAddress       VARCHAR(128),
-    Postcode                VARCHAR(10),
-    Phone                   VARCHAR(16)
-)
