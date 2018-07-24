e11f0ec17294dcc1e89e28cfde5231e99edbfdad
Julian Dragoi <julian_dragoi@hotmail.co.uk>
2018-07-24 11:16:43 +0100

    2 fact tables added for inspection + defects tables - however there is a duplicate key somewhere as it errors on execution

--- GasCertDW.sql
+++ GasCertDW.sql
@@ -1,43 +1,26 @@
 
-CREATE DATABASE IF NOT EXISTS GasCert_DW
+CREATE DATABASE IF NOT EXISTS GasCert_DW;
 
-
-USE GasCert_DW
-
-CREATE TABLE IF NOT EXISTS DimCustomer
-(
-	CustomerKey INT NOT NULL AUTO_INCREMENT,
-	FirstName VARCHAR(100),
-	Surname VARCHAR(100),
-	FirstLinAddress VARCHAR(100),
-	SecondLineAddress VARCHAR(100),
-	Postcode VARCHAR(10),
-	CompanyName VARCHAR(100),
-	Phone VARCHAR(16),
-	Email VARCHAR(300),
-	PRIMARY KEY (CustomerKey)
+CREATE TABLE IF NOT EXISTS DimApplianceType (
+ 	ApplianceTypeKey INT NOT NULL AUTO_INCREMENT,
+ 	App_Type VARCHAR(200) NOT NULL,
+ 	Make VARCHAR(200) NOT NULL,
+ 	Model VARCHAR(200) NOT NULL,
+ 	GCNumber VARCHAR(20),
+ 	PRIMARY KEY (ApplianceTypeKey)
 );
 
-CREATE TABLE IF NOT EXISTS DimUser
-(
-	UserKey INT NOT NULL AUTO_INCREMENT,
-	FirstName VARCHAR(100) NOT NULL,
-	LastName VARCHAR(100) NOT NULL,
-	GasID INT NOT NULL,
-	CompanyAddress VARCHAR(100) NOT NULL,
-	CompanyPostcode VARCHAR(10)NOT NULL,
-	CompanyGasID INT NOT NULL,
-	GasEngineerSigniture NVARCHAR(100)NOT NULL,
-	PRIMARY KEY (UserKey)
+CREATE TABLE IF NOT EXISTS DimFlueType(
+	FlueTypeKey  INT NOT NULL AUTO_INCREMENT,
+	FlueType VARCHAR(200) NOT NULL,
+	PRIMARY KEY (FlueTypeKey)
 );
-
 
 CREATE TABLE IF NOT EXISTS DimAppliance (
 	ApplianceKey INT NOT NULL AUTO_INCREMENT,
 	ApplianceTypeKey INT ,
 	FlueTypeKey INT ,
 	Location  VARCHAR(200),
-	LandlordAppliance VARCHAR(100),
 	CompanyPostcode BIT,
 	ApplianceInspected BIT,
 	OperatingPressure   DECIMAL,
@@ -51,27 +34,43 @@ CREATE TABLE IF NOT EXISTS DimAppliance (
 	ApplianceSafeToUse  BIT,
 	SpillageTest        BIT,
 	FlueFlowTest        BIT,
-	PRIMARY KEY (ApplianceKey)
-);
-
- 
-CREATE TABLE IF NOT EXISTS DimApplianceType (
- 	ApplianceTypeKey INT NOT NULL AUTO_INCREMENT,
- 	App_Type VARCHAR(200) NOT NULL,
- 	Make VARCHAR(200) NOT NULL,
- 	Model VARCHAR(200) NOT NULL,
- 	GCNumber VARCHAR(20),
- 	PRIMARY KEY (ApplianceTypeKey)
-);
-
- 
-CREATE TABLE IF NOT EXISTS DimFlueType(
-	FlueTypeKey  INT NOT NULL AUTO_INCREMENT,
-	FlueType VARCHAR(200) NOT NULL,
-	PRIMARY KEY (FlueTypeKey)
+	PRIMARY KEY (ApplianceKey),
+	FOREIGN KEY FK_FlueTypeKey(FlueTypeKey) REFERENCES DimFlueType(FlueTypeKey),
+	FOREIGN KEY FK_ApplianceTypeKey(ApplianceTypeKey) REFERENCES DimApplianceType(ApplianceTypeKey)
 );
 
 
+CREATE TABLE IF NOT EXISTS PersonType(
+PersonTypeKey INT NOT NULL AUTO_INCREMENT,
+Type VARCHAR(200),
+PRIMARY KEY(PersonTypeKey)
+);
+
+CREATE TABLE IF NOT EXISTS Address(
+	AddressKey INT NOT NULL AUTO_INCREMENT,
+	CompanyName VARCHAR(200),
+	FirstLineAddress VARCHAR(200),
+	SecondLineAddress VARCHAR(500),
+	Postcode VARCHAR(8),
+	PRIMARY KEY(AddressKey)
+);
+
+CREATE TABLE IF NOT EXISTS DimPerson
+(
+	PersonKey INT NOT NULL AUTO_INCREMENT,
+	PersonTypeKey INT NOT NULL,
+	AddressKey INT NOT NULL,
+	FirstName VARCHAR(100),
+	Surname VARCHAR(100),
+	CompanyName VARCHAR(100),
+	Phone VARCHAR(16),
+	Email VARCHAR(300),
+	GasID INT NOT NULL,
+	GasEngineerSigniture BLOB,
+	PRIMARY KEY (PersonKey),
+	FOREIGN KEY FK_PersonTypeKey(PersonTypeKey) REFERENCES PersonType(PersonTypeKey),
+	FOREIGN KEY FK_AddressKey(AddressKey) REFERENCES Address(AddressKey)  
+);
 
 CREATE TABLE IF NOT EXISTS DimCertification (
 	CertificationKey INT NOT NULL AUTO_INCREMENT,
@@ -81,7 +80,7 @@ CREATE TABLE IF NOT EXISTS DimCertification (
 	SmokeAlarm BIT,
 	SmokeFitted BIT,
 	SmokeTested BIT,
-	Defects VARCHAR(4000),
+	NumberOfDefects INT,
 	ECV BIT,
 	TightnessTest BIT,
 	PipeVisual BIT,
@@ -94,82 +93,58 @@ CREATE TABLE IF NOT EXISTS DimCertification (
 );
 
 
-CREATE TABLE IF NOT EXISTS DimJob (
-	JobKey INT NOT NULL AUTO_INCREMENT,
-	FirstName VARCHAR(128),
-	Surname VARCHAR(128),
-	FirstLineAddress VARCHAR(128),
-	SecondLineAddress VARCHAR(128),
-	PostCode VARCHAR(10),
-	Phone VARCHAR(16),
-	PRIMARY KEY (JobKey)
+CREATE TABLE IF NOT EXISTS DefectType(
+	DefectTypeKey INT NOT NULL AUTO_INCREMENT,
+	Type VARCHAR(200),
+	PRIMARY KEY(DefectTypeKey)
 );
 
 
-CREATE TABLE IF NOT EXISTS DimDate(
-	DateKey INT AUTO_INCREMENT, 
-	Date DATETIME,
-	FullDateUK CHAR(10),
-	DayOfMonth VARCHAR(2),
-	DaySuffix VARCHAR(4),
-	DayName VARCHAR(9),
-	DayOfWeekUK CHAR(1),
-	DayOfWeekInMonth VARCHAR(2),
-	DayOfWeekInYear VARCHAR(2),
-	DayOfQuarter VARCHAR(3),
-	DayOfYear VARCHAR(3),
-	WeekOfMonth VARCHAR(1),
-	WeekOfQuarter VARCHAR(2),
-	WeekOfYear VARCHAR(2),
-	Month VARCHAR(2), 
-	MonthName VARCHAR(9),
-	MonthOfQuarter VARCHAR(2),
-	Quarter CHAR(1),
-	QuarterName VARCHAR(9),
-	Year CHAR(4),
-	YearName CHAR(7), 
-	MonthYear CHAR(10),
-	MMYYYY CHAR(6),
-	FirstDayOfMonth DATE,
-	LastDayOfMonth DATE,
-	FirstDayOfQuarter DATE,
-	LastDayOfQuarter DATE,
-	FirstDayOfYear DATE,
-	LastDayOfYear DATE,
-	IsWeekday BIT,
-	IsHolidayUK BIT Null,
-	HolidayUK VARCHAR(50) Null,
-	PRIMARY KEY (DateKey)
+CREATE TABLE IF NOT EXISTS Defect(
+	DefectKey INT NOT NULL AUTO_INCREMENT,
+	DefectTypeKey INT NOT NULL,
+	Severity INT,
+	Comment VARCHAR(4000),
+	PRIMARY KEY(DefectKey),
+	FOREIGN KEY FK_DefectTypeKey(DefectTypeKey) REFERENCES DefectType(DefectTypeKey)
 );
 
- 
-CREATE TABLE IF NOT EXISTS DimTime( 
-	TimeKey int NOT NULL AUTO_INCREMENT, 
-	TimeAltKey int NOT NULL, 
-	Time30 varchar(8) NOT NULL, 
-	Hour30 tinyint NOT NULL, 
-	MinuteNumber tinyint NOT NULL, 
-	SecondNumber tinyint NOT NULL, 
-	TimeInSecond int NOT NULL, 
-	HourlyBucket varchar(15)not null, 
-	DayTimeBucketGroupKey int not null, 
-	DayTimeBucket varchar(100) not null,
-	PRIMARY KEY (TimeKey)
-);
- 
 
-
-CREATE TABLE IF NOT EXISTS FactCertificate(
-	FactCertificateKey INT NOT NULL AUTO_INCREMENT,
-	UserKey INT NOT NULL,
-	CustomerKey INT NOT NULL,
-	ApplianceKey INT NOT NULL,
+CREATE TABLE IF NOT EXISTS InspectionDefect(
+	InspectionDefectKey INT NOT NULL AUTO_INCREMENT,
 	CertificationKey INT NOT NULL,
-	JobKey INT NOT NULL,
-	DateKey INT NOT NULL,
-	TimeKey INT NOT NULL,
-	PRIMARY KEY (FactCertificateKey)
+	DefectKey INT NOT NULL,
+	PRIMARY KEY(InspectionDefectKey),
+	FOREIGN KEY FK_InspectionDefectKey(DefectKey) REFERENCES Defect(DefectKey)
 );
+
+
+CREATE TABLE IF NOT EXISTS FactInspection(
+	FactInspectionKey INT NOT NULL AUTO_INCREMENT,
+	PersonKey INT NOT NULL,
+	AddressKey INT NOT NULL,
+	NextInspectionDateTime DATETIME NOT NULL,
+	CertificationIssued BIT NOT NULL,
+	CertificationKey INT,
+	PRIMARY KEY (FactInspectionKey),
+	FOREIGN KEY FK_FactPersonKey(PersonKey) REFERENCES DimPerson(PersonKey),
+	FOREIGN KEY FK_AddressKey(AddressKey) REFERENCES Address(AddressKey)
+);
+
+
+
+CREATE TABLE IF NOT EXISTS FactInspectionAppliance(
+	FactInspectionApplianceKey INT NOT NULL AUTO_INCREMENT,
+	FactInspectionKey INT NOT NULL,
+	ApplianceKey INT NOT NULL,
+	LandlordAppliance BIT NOT NULL,
+	PRIMARY KEY(FactInspectionApplianceKey),
+	FOREIGN KEY FK_FactInspectionKey(FactInspectionKey) REFERENCES FactInspection(FactInspectionKey),
+	FOREIGN KEY FK_FactApplianceKey(ApplianceKey) REFERENCES DimAppliance(ApplianceKey)
+
+);
+
+
 
 
 /*-------------------------------------
