c86c07b4c6bfc5f2f73da0bc6714c34a267fe40a
Julian Dragoi <julian_dragoi@hotmail.co.uk>
2018-08-23 14:26:40 +0100

    added MySQL route

--- REST_API.py
+++ REST_API.py
@@ -3,18 +3,32 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 from flask_pymongo import PyMongo
+from flaskext import MySQL
 
 app = Flask(__name__)
 
-app.config['MONGO_DBNAME'] = 'GasCertDW'
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/GasCertDW'
+mysql = MySQL()
 
-mongo = PyMongo(app)
+app.config['MYSQL_DATABASE_USER'] = 'my_user'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'my_password'
+app.config['MYSQL_DATABASE_DB'] = 'my_database'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+#mongo = PyMongo(app)
+
+# app.config['MONGO_DBNAME'] = 'GasCertDW'
+# app.config['MONGO_URI'] = 'mongodb://localhost:27017/GasCertDW'
+
+mysql.init_app(app)
 
 
 @app.route('/')
-def root():
-    return "this is the root directory"
+def get():
+    cur = mysql.connect().cursor()
+    cur.execute('''select * from my_database.my_table''')
+    r = [dict((cur.description[i][0], value)
+              for i, value in enumerate(row)) for row in cur.fetchall()]
+    return jsonify({'myCollection' : r})
 
 
 @app.route('/ApplianceType', methods=['GET'])
