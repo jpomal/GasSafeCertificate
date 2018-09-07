e4424332e42036673a876af9831475b005fd48bc
Jiten pomal <jitz@Jitens-MBP.lan>
2018-09-07 11:34:54 +0200

    Changed Use DB

---
+++ SP_Insert_Address.sql
@@ -0,0 +1,63 @@
+-- JITEN.POMAL VERSION 01
+
+
+USE TEMPDB
+
+GO
+
+-- CREATE TABLE Address (
+-- 	AddressKey 				INT NOT NULL IDENTITY(1,1)			,
+-- 	CompanyName 			VARCHAR(200)						,
+-- 	FirstLineAddress 		VARCHAR(200)						,
+-- 	SecondLineAddress 	 	VARCHAR(500)						,
+-- 	Postcode 				VARCHAR(8)							,
+-- 	PRIMARY KEY(AddressKey)
+-- );
+
+
+CREATE PROCEDURE SP_Address
+
+	@AddressKey 			INT NOT NULL 						,
+	@CompanyName 			VARCHAR(200)						,
+	@FirstLineAddress 		VARCHAR(200)						,
+	@SecondLineAddress 	 	VARCHAR(500)						,
+	@Postcode 				VARCHAR(8)							,
+
+ AS
+BEGIN
+	
+	SET NOCOUNT ON
+
+	SET IDENTITY_INSERT Address ON;
+
+INSERT INTO Address
+
+	(
+		AddressKey						,
+		CompanyName						,
+		FirstLineAddress				,
+		SecondLineAddress				,
+		Postcode						,
+	)
+VALUES
+
+	(
+		@AddressKey						,
+		@CompanyName					,
+		@FirstLineAddress				,
+		@SecondLineAddress				,
+		@Postcode						,
+
+	)
+END
+
+
+EXEC SP_Address
+
+		@AddressKey	 =					,
+		@CompanyName =					,
+		@FirstLineAddress =				,
+		@SecondLineAddress =			,
+		@Postcode	=					,
+
+		
